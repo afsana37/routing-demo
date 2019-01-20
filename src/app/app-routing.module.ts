5cf9c23352148ea7197f5a31de11e43b3324781f
iamafsana <afsanaabida@yahoo.com>
2019-01-20 11:24:21 +0600

    childroutes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,23 @@ import { DepartmentListComponent } from './department-list/department-list.compo
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { DepartmentDetailComponent } from './department-detail/department-detail.component';
+import { DepartmentOverviewComponent } from './department-overview/department-overview.component';
+import { DepartmentContactComponent } from './department-contact/department-contact.component';
 
 const routes: Routes = [
   {path:'', redirectTo: '/departments', pathMatch:'full'},
   { path: 'departments', component: DepartmentListComponent},
-  {path:'departments/:id', component: DepartmentDetailComponent},
+
+  {
+    path:'departments/:id', 
+    component: DepartmentDetailComponent,
+    children: [
+
+      {path: 'overview', component:DepartmentOverviewComponent},
+      {path: 'contact', component:DepartmentDetailComponent}
+    ]
+    
+},
   { path: 'employees', component: EmployeeListComponent},
   {path: "**",component: PageNotFoundComponent}
 ];
@@ -21,4 +33,6 @@ export class AppRoutingModule { }
 export const routingComponents = [DepartmentListComponent, 
                                   EmployeeListComponent,
                                   PageNotFoundComponent,
-                                  DepartmentDetailComponent];
+                                  DepartmentDetailComponent,
+                                  DepartmentOverviewComponent,
+                                  DepartmentContactComponent];
